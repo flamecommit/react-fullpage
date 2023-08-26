18c65f73e5d926eb97c2cb33b2b0b421ac09c327
신용준 <shinyj1991@gmail.com>
2023-08-26 21:52:17 +0900

    feat : remove export Wrapper

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,3 +1,2 @@
-export { default as FullpageWrapper } from "./components/FullpageWrapper";
-export { default as FullpageContainer } from "./components/FullpageContainer";
-export { default as FullpageSection } from "./components/FullpageSection";
+export { default as FullpageContainer } from './components/FullpageContainer';
+export { default as FullpageSection } from './components/FullpageSection';
