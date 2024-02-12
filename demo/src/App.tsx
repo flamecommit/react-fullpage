64de837efa4b41f252adfdf7962a32ea265d5e6e
신용준 <shinyj1991@gmail.com>
2024-02-12 16:03:26 +0900

    test

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Fullpage from './routes/Fullpage';
 import Home from './routes/Home';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/fullpage" element={<Fullpage />} />
     </Routes>
   );
 }
