3ad0cd91937db240ed37fc86abb220581a2d1b25
신용준 <shinyj1991@gmail.com>
2024-02-12 14:30:58 +0900

    feat: demo

---
+++ demo/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Fullpage from './routes/Fullpage';
+import Home from './routes/Home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/fullpage" element={<Fullpage />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ demo/src/components/ThirdSection.tsx
@@ -0,0 +1,113 @@
+import * as React from 'react';
+
+function ThirdSection() {
+  return (
+    <div
+      className="third-section"
+      style={{
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '80px 0 0',
+      }}
+    >
+      <div
+        className="outer"
+        style={{
+          width: '100%',
+          backgroundColor: 'orange',
+          padding: '80px 0 0',
+        }}
+      >
+        <div className="inner" style={{ padding: '60px 0' }}>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Third Section</p>
+          <p>Last</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ThirdSection;
